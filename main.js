28cd7e999b675513beb6cec4d4f776c93648db18
fabian-gubler <fabian.gubler@hotmail.com>
2021-10-19 22:51:10 +0200

    update

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+// Data Structures
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+}
+
+let testBook = new Book("Hobbit", "Jackson", "200", "Read")
+myLibrary.push(testBook)
+
+console.log(myLibrary)
+
+
 // User Interface
 const button = document.getElementById("button")
 const modal = document.getElementById("modal")
 const closeButton = document.querySelector(".closeButton")
+const submitButton = document.getElementById("submit")
 
 // Interactive
 button.addEventListener("click", modalOpen)
 closeButton.addEventListener("click", modalClose)
+submitButton.onsubmit = submitBook
 
 function modalOpen () {
   console.log("Modal Opened")
@@ -16,3 +36,8 @@ function modalClose () {
   console.log("Modal Closed")
   modal.style.display = "none";
 } 
+
+function submitBook () {
+  console.log("Book submitted")
+  modal.style.display = "none";
+}
